712f28c3f303d8c65a8772fedcbb59d2e9f895d4
satheem <satheem511@gmail.com>
2025-11-30 21:53:10 +0530

    Created folder p009

--- p008/p008.py
+++ p008/p008.py
@@ -1,5 +1,6 @@
 N = int(input("Enter a no: ")) + 1
 
+# Numbers Divisible by (3 or 5) and not divide by 15
 for no in range(1,N):
     if (no % 3 == 0 or no % 5 == 0) and no % 15 != 0:
         print(no)
