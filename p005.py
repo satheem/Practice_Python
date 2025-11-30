bb15bd284e7899a464b29e9aac6f95cdef29abb4
satheem <satheem511@gmail.com>
2025-11-30 18:31:16 +0530

    solved problem p005.md and writen code in file p005.py

--- p005.py
+++ p005.py
@@ -2,7 +2,7 @@ a, b, c = input("Enter 3 Numbers with space: ").split()
 num_lis = [int(a), int(b), int(c)]
 
 largest = smallest = num_lis[0]
-is_posi = "No"
+is_posi = "Yes"
 is_valid = "No"
 for no in num_lis:
     # Largest
@@ -12,11 +12,11 @@ for no in num_lis:
     if smallest > no:
         smallest = no
     # Is positive 
-    if no > 0:
-        is_posi = "Yes"
-    # Is valid triangle
-    if int(a) + int(b) > int(c) and int(a) + int(c) > int(b) and int(b) + int(c) > int(a):
-        is_valid = "Yes"
+    if no < 0:
+        is_posi = "No"
+# Is valid triangle
+if int(a) + int(b) > int(c) and int(a) + int(c) > int(b) and int(b) + int(c) > int(a):
+    is_valid = "Yes"
 
 print(f"Largest {largest}")
 print(f"Smallest {smallest}")
