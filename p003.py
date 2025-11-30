9a3cbfb0c198263d33c914a96b128f6e2fdd9d29
satheem <satheem511@gmail.com>
2025-11-30 13:46:38 +0530

    Solve p003.py

--- p003.py
+++ p003.py
@@ -1,2 +1,30 @@
-for i in range(0,3):
-    no = input("Enter three numbers: ")
+x,y,z = input("Enter three number with spaces: ").split()
+num_lis = [int(x),int(y),int(z)]
+
+largest = smallest  = num_lis[0]
+total = 0
+triangle_typ = ""
+
+for no in num_lis:
+    # Largest
+    if largest < no:
+        largest = no
+    # Smallest
+    if smallest > no:
+        smallest = no
+    
+# middle 
+    total += no
+middle = total-(largest+smallest)
+
+if num_lis[0] == num_lis[1] == num_lis[2]:
+    triangle_typ = "equilateral triangle"
+elif num_lis[0] == num_lis[1] or num_lis[1] == num_lis[2] or num_lis[0] == num_lis[2]:
+    triangle_typ = "isosceles triangle"
+else:
+    triangle_typ = "scalene triangle"
+    
+print(f"Largest: {largest}")
+print(f"Middle: {middle}")
+print(f"Smallest: {smallest}")
+print(f"Triangle type: {triangle_typ}")
