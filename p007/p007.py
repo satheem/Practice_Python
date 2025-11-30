ca9380edb316d5a7c2ff729088bbf6922fae119b
satheem <satheem511@gmail.com>
2025-11-30 20:37:58 +0530

    Solved p007

---
+++ p007/p007.py
@@ -0,0 +1,11 @@
+# for loop version
+N = int(input("Enter a No: "))
+
+for no in range(N,0,-1):
+    print(no)
+
+# while loop version
+N = int(input("Enter a No: "))
+while N > 0:
+    print(N)
+    N -= 1
